eb70118de873531701776af3a2f89b2846acc94e
Brad Schrag <brad.schrag@gmail.com>
2018-11-22 12:26:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a + b <= c || a + c <= b || b + c <= a
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        error.message
+      end
+    elsif @a == @b
+      if @b == @c
+        :equilateral
+      else
+        :isosceles
+      end
+    elsif @b == @c || @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+      message = "Not a valid triangle"
+  end
 end
